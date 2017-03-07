5a3c0c3095ed473b4ddef99e0d69a298a6ca36ad
kylem0524 <blmendelson@Brads-MacBook-Air.local>
2017-03-07 14:07:53 -0500

    moving ellipse
    uploaded documents to github

---
+++ sketch.js
@@ -0,0 +1,8 @@
+function setup() {
+createCanvas(400,400); //p1 changes the width, p2 changes the height
+}
+
+function draw() {
+background(mouseX, 150, 00); //p1 changes the color
+ellipse(mouseX,mouseY,300,300);
+}
